abd104e043f2d90b8bb4ace89a126385cda743be
Clara C <clara.casas.castedo@gmail.com>
2016-11-10 21:06:42 +0100

    Include url from diapason server

--- alexawrap.py
+++ alexawrap.py
@@ -4,17 +4,21 @@ import traceback
 app = Flask(__name__)
 
 #Template of the response required by Alexa - JSON format
-toAlexaTemplate = '''{
+toAlexaTemplate = '''
+{
   "version": "1.0",
   "response": {
     "outputSpeech": {
-      "type": "SSML",
-      "ssml": "<speak><audio src="https://%s.mp3" /></speak>"
-    }
-  }
+    "type": "SSML",
+    "ssml": "<speak><audio src='https://diapason.reset.etsii.upm.es/v0/alexa/%s.mp3' /></speak>"
+    },
+    "shouldEndSession": true
+  },
+  "sessionAttributes": {}
 }
-'''
 
+'''
+#<audio src="https://diapason.reset.etsii.upm.es/v0/alexa/B.mp3"/>
 @app.route('/token', methods=['POST'])
 def post_handler():
     if request.method == 'POST':
