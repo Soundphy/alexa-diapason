8e0fb1dd687773b4bf9de60c8de735243c2ee8df
Clara C <clara.casas.castedo@gmail.com>
2016-11-12 17:18:36 +0100

    Include audio

--- alexawrap.py
+++ alexawrap.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
+DIAPASON_URL = "https://diapason.reset.etsii.upm.es/v0/alexa/"
 
 @ask.intent('GetMusicalNote', mapping={'note': 'Note'})
 def get_note(note):
-    text = "The note is %s" % note
+    DIAPASON_URL_NOTE = DIAPASON_URL + note
+    text = "<speak><audio src='%s' /></speak>" % DIAPASON_URL_NOTE
     return statement(text).simple_card('Turning fork', text)
 
 
